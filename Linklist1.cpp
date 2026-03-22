db7434e9184357b180477eaaeab4f03639843d93
icy_w <icy_wcz@sjtu.edu.cn>
2026-03-22 17:16:44 +0800

    implement stack class and parentheses matching algorithm

--- Linklist1.cpp
+++ Linklist1.cpp
@@ -6,7 +6,7 @@ class linklist
     struct Node
     {
         elemtype data;
-        elemtype *next;
+        Node *next;
         Node(elemtype data_=0)
         {
             data=data_;
@@ -92,6 +92,81 @@ public:
 
     }
 
+    elemtype getlastkval(int k)
+    {
+        Node *p=head;
+        Node *q=head;
+        for (int i =0;i<k-1;i++)
+        {
+            while (p->next !=nullptr)
+            {
+                p=p->next;
+
+            }
+        }
+        while (p->next !=nullptr)
+        {
+            p=p->next;
+            q=q->next;
+
+        }
+        return q->data;
+    }
+
+    void merge(linklist &c1,linklist &c2)
+    {
+        Node *p=c1.head->next;
+        Node *q=c2.head->next;
+        Node *s=c1.head;
+        c2.head->next=nullptr;
+        while (p!=nullptr && q!=nullptr)
+        {
+            if (p->data>q->data)
+            {
+                s->next=q;
+                q=q->next;
+                s=s->next;
+            }
+            else
+            {
+                s->next=p;
+                p=p->next;
+                s=s->next;
+            }
+        }
+        if (p!=nullptr)
+        {
+            s->next=p;
+        }
+        if (q !=nullptr)
+        {
+            s->next=q;
+        }
+
+    }
+    bool cycle()
+    {
+        Node *p=head;
+        Node *q=head;
+        while (p->next!=nullptr && p->next->next!=nullptr)
+        {
+            p=p->next->next;
+            q=q->next;
+            if (p==q)
+            {
+                p=head;
+                while (p!=q)
+                {
+                    p=p->next;
+                    q=q->next;
+                }
+
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 };
 
@@ -155,3 +230,16 @@ public:
 
     }
 };
+
+int main()
+{
+    linklist <int> a;
+    linklist <int> b;
+    a.inserttail(1);
+    a.inserttail(2);
+    a.inserttail(3);
+    b.inserttail(4);
+    b.inserttail(5);
+    a.merge(a,b);
+    a.show();
+}
