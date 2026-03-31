5ed5cb92931284d8bd747d258ca4d98717189f4c
icy_w <icy_wcz@sjtu.edu.cn>
2026-03-31 16:04:54 +0800

    refactor queue class and add comments for clarity

--- queue.cpp
+++ queue.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
 using namespace std;
 template <class elemtype>
-class queue
+class queue//循环队列的实现
 {
 private:
     elemtype *p;
     int maxsize;
-    int front;
-    int rear;
+    int front; //指向队头元素的前一个位置
+    int rear;// 指向队尾的元素
     void doublespace()
     {
         elemtype *tem=p;
         p=new elemtype[2*maxsize];
-        for (int i=1;i<=maxsize;i++)
+        for (int i=1;i<=maxsize;i++) //因为front指向的是队头的前一个元素 所以i从一开始
         {
-            p[i]=tem[(front+i)%maxsize];
+            p[i]=tem[(front+i)%maxsize]; //循环队列取模运算
 
         }
-        front=0;
-        rear=maxsize;
+        front=0; //指向队头的前一个位置
+        rear=maxsize; //指向队尾
         maxsize*=2;
         delete []tem;
 
@@ -43,7 +43,7 @@ public:
     }
     void enqueue(const elemtype &x)
     {
-        if ((rear+1)%maxsize==front)
+        if ((rear+1)%maxsize==front) //rear紧挨着front说明队列满了 需要扩容
         {
             doublespace();
 
@@ -55,7 +55,7 @@ public:
     {
         if (isEmpty())
         {
-            return -1;
+            return -1; //空队列不能执行出队操作 返回-1表示出队失败
         }
         front=(front+1)%maxsize;
         return p[front];
@@ -72,7 +72,7 @@ public:
 };
 
 template <class elemtype>
-class linkqueue
+class linkqueue //链接队列的实现
 {
 private:
     struct Node
@@ -129,7 +129,7 @@ public:
             return -1;
         }
         elemtype val =front->data;
-        front=front->next;
+        front=front->next; //移动front指针至下一个节点
         if (front==nullptr)
         {
             rear=nullptr;
