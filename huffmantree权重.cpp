b5a8e06448b8a4ef55e90db4ecc856d9c8a36aee
icy_w <icy_wcz@sjtu.edu.cn>
2026-05-05 20:10:10 +0800

    add comments to clarify depth-first search logic and weight calculation in huffman tree implementation

--- huffmantree权重.cpp
+++ huffmantree权重.cpp
@@ -4,7 +4,6 @@
 
 #include<iostream>
 using namespace std;
-#include<queue>
 
 
 struct node
@@ -18,21 +17,21 @@ struct node
 long long wpl=0;
 
 
-void dfs(int root ,int depth,node arr[])
+void dfs(int root ,int depth,node arr[]) //使用递归算法计算depth
 {
-    if (arr[root].left==0 && arr[root].right==0)
+    if (arr[root].left==0 && arr[root].right==0) //计算叶子节点的weight
     {
         wpl+=arr[root].weight*depth;
         return ;
 
     }
 
-    if (arr[root].left!=0)
+    if (arr[root].left!=0) //计算该节点的左子树的wpl
     {
         dfs(arr[root].left,depth+1,arr);
 
     }
-    if (arr[root].right!=0)
+    if (arr[root].right!=0) //计算该节点的右子树的wpl
     {
         dfs(arr[root].right,depth+1,arr);
     }
@@ -53,7 +52,7 @@ int main()
         cin>>arr[i].right;
 
     }
-    bool is_child[n+1]={false};
+    bool is_child[n+1]={false}; //找到根节点
     for (int i=1;i<n+1;i++ )
     {
         if (arr[i].left!=0)
